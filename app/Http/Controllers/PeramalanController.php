36f3e8df15f4ec01cf789dc8e1eee246ba272675
firchun025 <firchun025@gmail.com>
2024-06-26 22:55:28 +0900

    update peramalan

--- app/Http/Controllers/PeramalanController.php
+++ app/Http/Controllers/PeramalanController.php
@@ -246,7 +246,7 @@ class PeramalanController extends Controller
                 $error_2 = 0;
             } else {
 
-                $error_2 = round(($error / $aktual_bulan_ini) * 100, 0);
+                $error_2 = round(($error / $aktual_bulan_ini) * 100);
                 // $error_2 = round(($error / $aktual_bulan_ini) * 100, 2);
             }
             $peramalanData = [
